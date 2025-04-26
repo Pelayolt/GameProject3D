83e61a8bfee398829686070d8d17050050e1f29f
pelayolopez <pelayo.lopez@udc.es>
2025-04-26 18:03:47 +0200

    Mejora movimiento tanque

--- Assets/Scripts/Tank/PlayerMovement.cs
+++ Assets/Scripts/Tank/PlayerMovement.cs
@@ -11,28 +11,49 @@ public class PlayerMovement : MonoBehaviour
     public float frontWheelsAngle = 30f;
     public float currentFrontWheelsAngle = 0f;
 
+    public float moveSpeed = 10f;
+
     public Transform[] wheels;
-    
+
     public Transform frontRightWheel;
     public Transform frontLeftWheel;
 
 
-    void Update(){
+    void Update()
+    {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
-        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
-        
-        foreach (Transform wheel in wheels){
-            wheel.Rotate(Vector3.right, verticalInput * rotationSpeed * Time.deltaTime);
+        // Movimiento del tanque
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.up * moveSpeed * horizontalInput * turnSpeed * Time.deltaTime);
+
+        // Ruedas girando
+        foreach (Transform wheel in wheels)
+        {
+            float wheelRotation = verticalInput * rotationSpeed * Time.deltaTime;
+
+            // Si está girando, sumar o restar según el lado de la rueda
+            if (verticalInput == 0 && horizontalInput != 0)
+            {
+                if (wheel.name.Contains("left"))
+                {
+                    wheelRotation = horizontalInput * rotationSpeed * Time.deltaTime;
+                }
+                else if (wheel.name.Contains("right"))
+                {
+                    wheelRotation = -horizontalInput * rotationSpeed * Time.deltaTime;
+                }
+            }
+
+            wheel.Rotate(Vector3.right, wheelRotation);
         }
 
+        // Direccionar las ruedas delanteras
         currentFrontWheelsAngle = horizontalInput * frontWheelsAngle;
-
         frontLeftWheel.localRotation = Quaternion.Euler(0f, currentFrontWheelsAngle, 0f);
         frontRightWheel.localRotation = Quaternion.Euler(0f, currentFrontWheelsAngle, 0f);
-        
     }
-    
+
+
 }
